f58ae863a8d817e6d132e579f3e2bb3df5b4b00c
yousaf wazir <umbrellaman612@gmail.com>
2022-10-19 14:31:33 +0100

    filter method

---
+++ filter.js
@@ -0,0 +1,15 @@
+/* filter method */
+var array = [1, 2, 3, 4, 5, 6];
+
+var evenArray = array.filter((t) => t % 2);
+
+console.log(evenArray);
+
+/*
+
+none mutative 
+
+Returns the elements of an array that meet the 
+condition specified in a callback function.
+
+*/
