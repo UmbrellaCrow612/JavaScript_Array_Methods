5d58376e5f0922b3ba49c35264d40ea436386455
yousaf wazir <umbrellaman612@gmail.com>
2022-10-19 11:56:50 +0100

    added slice copy array to point

---
+++ slice.js
@@ -0,0 +1,16 @@
+/* slice method */
+var array = [1, 2, 3, 4, 5, 6];
+
+var newA = array.slice();
+
+console.log(newA);
+
+/*
+
+none mutative 
+
+return a copy of an array from a starting point to an end point
+
+up to the end point but not including it
+
+*/
