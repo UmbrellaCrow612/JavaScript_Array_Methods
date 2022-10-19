77fb148c91190fb49ab968ecc716545477a63701
yousaf wazir <umbrellaman612@gmail.com>
2022-10-19 12:00:16 +0100

    added foreach

---
+++ forEach.js
@@ -0,0 +1,16 @@
+/* forEach method */
+var array = [1, 2, 3, 4, 5, 6];
+
+console.log(
+  array.forEach((item, index, array) =>
+    console.log(item + " at index" + index + " array at " + array)
+  )
+);
+
+/*
+
+none mutative 
+
+
+
+*/
