8813186551058a2e4bd9b6c2367a1a540d5320d7
yousaf wazir <umbrellaman612@gmail.com>
2022-10-19 11:05:52 +0100

    added pop - rem last ele

--- join.js
+++ join.js
@@ -7,6 +7,10 @@ console.log(array.join("@"))
 
 /*
 
+None mutative 
+
+return a stringified array, with the passed separator between elements  
+
 A string used to separate one element of the 
 array from the next in the resulting string. 
 If omitted, the array elements are separated with a comma.
---
+++ pop.js
@@ -0,0 +1,17 @@
+/* pop method */
+var array = [1, 2, 3];
+
+var newArray = array
+
+console.log(array.pop());
+console.log(newArray);
+
+/*
+
+
+MUTATIVE AND EFFECTS THE ORG ARRAY
+
+Removes the last element from an array and returns it
+
+
+*/
