fe9e104e1db62b946144e1e404acfdd1e28da188
yousaf wazir <umbrellaman612@gmail.com>
2022-10-19 14:33:14 +0100

    added map

---
+++ map.js
@@ -0,0 +1,16 @@
+/* filter method */
+var array = [1, 2, 3, 4, 5, 6];
+
+var ar = array.map((item) => item * 2);
+
+console.log(ar);
+
+/*
+
+none mutative 
+
+Calls a defined callback 
+function on each element of an array, and returns 
+an array that contains the results.
+
+*/
