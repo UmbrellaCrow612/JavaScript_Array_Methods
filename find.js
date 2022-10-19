e09130d1ec3aec12f1092874a4c21b8c7dd360a4
yousaf wazir <umbrellaman612@gmail.com>
2022-10-19 14:25:35 +0100

    added find meth

---
+++ find.js
@@ -0,0 +1,15 @@
+/* find method */
+var array = [1, 2, 3, 4, 5, 6];
+
+array.find((element, index, array) => {
+  /* … */
+});
+
+/*
+
+none mutative 
+
+Returns the value of the first 
+element in the array where predicate is true, and undefined otherwise.
+
+*/
--- intro.js
+++ intro.js
@@ -1,15 +1,4 @@
-
-
-var array = []
-
-// This is an array
-
 /*
-Array can be contain lots of  multiple types 
 
-Like bool
-numbers
-strings
-objects chars
-
+Use mozilla docs and provide example and explanation to understand it
 */
