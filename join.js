db219f975199a1fda765b30f2d896676d8a37d0d
yousaf wazir <umbrellaman612@gmail.com>
2022-10-19 11:00:52 +0100

    added join method

---
+++ join.js
@@ -0,0 +1,14 @@
+/* Join method */
+
+var array = [1, 2, 3, 4]
+
+console.log(array.join("@"))
+
+
+/*
+
+A string used to separate one element of the 
+array from the next in the resulting string. 
+If omitted, the array elements are separated with a comma.
+
+*/
