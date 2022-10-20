712aafbd6f7ea6dce88e2bc27de96a3a7021f3fc
yousaf wazir <umbrellaman612@gmail.com>
2022-10-20 09:06:53 +0100

    added splice

---
+++ concat/splice.js
@@ -0,0 +1,16 @@
+/* splice method */
+var array = [1, 2, 3];
+
+var deletedItems = array.splice(0, 2, "a", "b");
+
+console.log(array);
+console.log(deletedItems);
+
+/*
+
+mutative
+
+Removes elements from an array and, if necessary, 
+inserts new elements in their place, returning the deleted elements.
+
+*/
