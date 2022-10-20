27fca7da702ac89c4ef1d3580d880aa13a299b2e
yousaf wazir <umbrellaman612@gmail.com>
2022-10-20 08:55:04 +0100

    added fill

---
+++ concat/fill.js
@@ -0,0 +1,16 @@
+/* fill method */
+var array = [1, 2, 3];
+
+console.log(array.fill("new value", 0, 2))
+console.log(array);
+
+/*
+mutative 
+
+value to fill array section with
+
+
+Changes all array elements from start 
+to end index to a static value and returns the modified array
+
+*/
