f05a85c1d0773fa33d3e3dd93b6ebc4726d849da
yousaf wazir <umbrellaman612@gmail.com>
2022-10-20 08:53:06 +0100

    added concat

---
+++ concat/concat.js
@@ -0,0 +1,15 @@
+/* contact method */
+var array = [1, 2, 3];
+
+console.log(array.concat([4, 5, 6]));
+console.log(array);
+
+/*
+
+None mutative 
+
+Additional arrays and/or items to add to the end of the array.
+This method returns 
+a new array without modifying any existing arrays.
+
+*/
